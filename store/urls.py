3e05faa82b70274055deb6cdc0a1968eb731ea1b
Gurini <gurinidev@gmail.com>
2024-02-13 10:56:09 +0100

    Minor bug fixes and js addition

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.store, name='store'),
     path('cart/', views.cart, name='cart'),
-    path('checkout/', views.checkout, name='checkout'), 
+    path('checkout/', views.checkout, name='checkout'),
+
+    #update
+    path('update_item/', views.upadateItem, name='update_item'),
+
+    #Process orer
+    path('proceess_order/', views.processOrder, name='proceess_order'),
 ]
